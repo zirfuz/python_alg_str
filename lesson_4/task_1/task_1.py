106384ee8bf173d9b7aad592e1d9dccf74e82401
AO7 <asdf7@asdf.asdf>
2019-01-13 17:44:20 +0300

    Add conclusion

--- lesson_4/task_1/task_1.py
+++ lesson_4/task_1/task_1.py
@@ -1,6 +1,8 @@
 # Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
 
 import timeit
+import cProfile
+import sys
 
 
 def sum_rec(n, x=1):
@@ -38,9 +40,24 @@ def test_sums(n):
 
 test_sums(500)
 
+
 file_sum_rec = open('sum_rec.txt', 'w')
 file_sum_loop = open('sum_loop.txt', 'w')
 
+# Вывод замеров в файлы
 for n in range(1, 500):
     print(timeit.timeit('sum_rec(n)', setup="from __main__ import sum_rec, n", number=100), file=file_sum_rec)
     print(timeit.timeit('sum_loop(n)', setup="from __main__ import sum_loop, n", number=100), file=file_sum_loop)
+
+
+# cProfile
+sys.setrecursionlimit(3000)
+cProfile.run('sum_rec(2900)')
+cProfile.run('sum_loop(10000000)')
+
+
+# Выводы:
+# Графики зависимости времени выполнения от величины n подтверждают линейную сложность обоих алгоритмов.
+# В данных реализациях решение через цикл заметно быстрее решения через рекурсию и не вызывает проблем с переполнением
+# стэка для большого n.
+# С помощью профилировщика я не выявил участков кода, поправив которые, можно существенно повысить быстродействие.
