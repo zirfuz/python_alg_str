0b2c994716197b3cbfdb8f2e32e699802dc8d4db
AO7 <asdf7@asdf.asdf>
2019-01-13 18:35:32 +0300

    Task2...

--- lesson_4/task_2/task_2.py
+++ lesson_4/task_2/task_2.py
@@ -1,18 +1,58 @@
+# Написать два алгоритма нахождения i-го по счёту простого числа.
+#   Без использования «Решета Эратосфена»;
+#   Используя алгоритм «Решето Эратосфена»
+
+def is_prime(value):
+    for i in range(2, value - 1):
+        if value % i == 0:
+            return False
+    return True
 
 
-n = 1000
+def prime(i):
+    value = 1
+    while i > -1:
+        value += 1
+        if is_prime(value):
+            i -= 1
+    return value
 
-sieve = [i for i in range(n)]
-# print(sieve)
-sieve[1] = 0
 
-for i in range(2, n):
-    if sieve[i] != 0:
-        j = i + i
-        while j < n:
-            sieve[j] = 0
-            j += i
 
-# print(sieve)
-result = [i for i in sieve if i != 0]
-print(result)
+class Sieve:
+    def __init__(self, size):
+        self._sieve = [i for i in range(size)]
+        self._sieve[1] = 0
+
+        for i in range(2, size):
+            if self._sieve[i] != 0:
+                j = i + i
+                while j < size:
+                    self._sieve[j] = 0
+                    j += i
+
+        self._sieve = [i for i in self._sieve if i != 0]
+
+    def get(self, i):
+        return self._sieve[i]
+
+
+sieve = Sieve(1000)
+
+
+def test_prime(i):
+    lst = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    if i < len(lst):
+        assert lst[i] == prime(i)
+        assert lst[i] == sieve.get(i)
+    else:
+        assert prime(i) == sieve.get(i)
+    print(f'Test {i} OK')
+
+
+def test_primes(n):
+    for i in range(n):
+        test_prime(i)
+
+
+test_primes(100)
