d290fdbfd58036463ab90f6c90a68750dc7ed5fb
AO7 <asdf7@asdf.asdf>
2019-01-31 22:31:51 +0300

    Task 1

--- lesson_6/task_1.py
+++ lesson_6/task_1.py
@@ -4,6 +4,7 @@
 #    Результаты анализа вставьте в виде комментариев к коду. Также укажите в комментариях версию Python и разрядность вашей ОС.
 
 import sys
+import random
 
 
 class PersistentLocals(object):
@@ -39,7 +40,7 @@ class PersistentLocals(object):
         print(f'TOTAL: {total}\n')
         for var, sz in _vars.items():
             print(f'{var}: {sz}')
-        print('=' * 20)
+        print()
 
     def _mem_size(self, x):
         total = sys.getsizeof(x)
@@ -55,11 +56,65 @@ class PersistentLocals(object):
         return total
 
 
+SIZE = 200
+MIN = 1
+MAX = 10
+
+ARRAY = [random.randint(MIN, MAX) for _ in range(SIZE)]
+ARRAY=[1,2,3]
+print(ARRAY)
+print()
+
+
 @PersistentLocals
-def f():
-    a = 1
-    b = [1, 2]
+def f1():
+    min1 = ARRAY[0]
+    min2 = ARRAY[1]
 
+    for i in range(2, len(ARRAY)):
+        if min1 > min2:
+            min1, min2 = min2, min1
+        item = ARRAY[i]
+        if item < min2:
+            min2 = min1
+            min1 = item
 
-f()
-f.print_mem()
+    if min1 > min2:
+        min1, min2 = min2, min1
+
+    return min1, min2
+
+@PersistentLocals
+def f2():
+    array = ARRAY.copy()
+    min1 = min(array)
+    array.remove(min1)
+    min2 = min(array)
+    return min1, min2
+
+@PersistentLocals
+def f3():
+    min1_i = ARRAY.index(min(ARRAY))
+    min2 = float('inf')
+    for i in range(len(ARRAY)):
+        value = ARRAY[i]
+        if i != min1_i and value < min2:
+            min2 = value
+    return ARRAY[min1_i], min2
+
+r1 = f1()
+r2 = f2()
+r3 = f3()
+
+assert(r1 == r2 == r3)
+
+f1.print_mem()
+f2.print_mem()
+f3.print_mem()
+
+# Выводы:
+# Алгоритмы f1 и f3 потребляют заметно меньше памяти чем f2.
+# f1 и f3 дают похожие результаты употребления памяти.
+# На примере из двух элементов (например ARRAY = [1, 2] f1 может употребить почти в 2 раза меньше памяти, чем f3.
+# На некоторых примерах (например ARRAY = [1, 2, 3]) f3 может употребить чуть меньше памяти, чем f1.
+# `array = ARRAY.copy()` потребляет существенное количество памяти.
