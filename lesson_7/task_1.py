5250fa71f5b10fc4675a0f13e10fade804f9c086
AO7 <asdf7@asdf.asdf>
2019-02-03 15:45:51 +0300

    Task_1

---
+++ lesson_7/task_1.py
@@ -0,0 +1,43 @@
+# 1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный массив,
+#    заданный случайными числами на промежутке [-100; 100).
+#    Выведите на экран исходный и отсортированный массивы. Сортировка должна быть реализована в виде функции.
+#    По возможности доработайте алгоритм (сделайте его умнее).
+
+import random
+
+MIN = -100
+MAX = 100
+LEN = 20
+
+
+def bubble_sort(array):
+    len_ar = len(array)
+    for i in range(len_ar - 1):
+        for j in range(i + 1, len_ar):
+            if array[i] < array[j]:
+                array[i], array[j] = array[j], array[i]
+
+
+def test():
+    array = [random.randint(MIN, MAX - 1) for _ in range(LEN)]
+    ar1 = array.copy()
+    ar2 = array.copy()
+    bubble_sort(ar1)
+    ar2 = sorted(ar2, reverse=True)
+    assert ar1 == ar2
+
+
+def test_n(n):
+    for i in range(n):
+        test()
+        print(f'test {i + 1: 4} OK')
+
+
+test_n(100)
+print()
+
+array = [random.randint(MIN, MAX - 1) for _ in range(LEN)]
+print(array)
+
+bubble_sort(array)
+print(array)
